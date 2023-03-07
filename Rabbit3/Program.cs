751b70763b9d6061a8454c82f0062ac80a6411ad
HNU <HNU@DESKTOP-8C4Q3SL>
2023-03-07 14:34:14 +0900

    초기 커밋.

---
+++ Rabbit3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Rabbit3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Write");
+            Console.Write("Wirte");
+            Console.Write("Wirte");
+            Console.Write("Wirte");
+            Console.Write("Wirte");
+            
+
+        }
+    }
+}
